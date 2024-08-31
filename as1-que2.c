9efee141b58d1cc0f404db18856331be93271765
Gayatri-0826 <ahergayatri2699@gmail.com>
2024-08-31 14:40:18 +0530

    my 1st assignment

---
+++ as1-que2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+  int num;
+
+  printf("Enter the number:"); 
+
+ 	 scanf("%d",&num);
+   printf("Numbe in Decimal:%d\n",num);	 
+   printf("Number in Character:%c\n",num);
+   printf("Number in Hexadecimal:%x\n",num);
+   printf("NUmber in Octal:%o\n",num);
+
+
+  return 0;
+}
---
+++ que4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+ int main()
+{ 
+	printf("*\n");
+	printf("*\t*\n*\t*\t*\n*\t*\t*\t*\n*\t*\t*\t*\t*\t\n");
+
+	return 0;
+}
